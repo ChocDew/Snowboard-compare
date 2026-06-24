e835d7027af4ca524dc8e823ccdb0f65ec4df5d1
ChocDew <140984672+ChocDew@users.noreply.github.com>
2026-06-24 20:25:57 +1000

    Replace next.config.ts with next.config.js for Vercel compatability

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Allows importing JSON files directly (used for the snowboards data)
-  experimental: {},
-};
-
-export default nextConfig;
